f6608a1cc546403d80710c1d05fdecb7eb8f40f7
Jonathan Tanuwijaya <jonathantw64@gmail.com>
2021-08-12 14:47:28 +0700

    added chapter and lesson endpoint

--- service-course/app/Models/Chapter.php
+++ service-course/app/Models/Chapter.php
@@ -12,7 +12,10 @@ class Chapter extends Model
     protected $fillable = [
         'name','course_id'
     ];
-
+    protected  $casts =[
+        'created_at'=> 'datetime:Y-m-d H:m:s',
+        'updated_at'=> 'datetime:Y-m-d H:m:s'
+    ];
     public function lessons(){
         return $this->hasMany(Lesson::class)->orderBy('id',"ASC");
     }
--- service-course/routes/api.php
+++ service-course/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ChapterController;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\LessonController;
 use App\Http\Controllers\MentorController;
 use Illuminate\Support\Facades\Route;
 
@@ -25,4 +27,16 @@ Route::get('courses',[CourseController::class,'index']);
 Route::post('courses',[CourseController::class,'create']);
 Route::put('courses/{id}',[CourseController::class,'update']);
 Route::delete('courses/{id}',[CourseController::class,'destroy']);
+
+Route::post('chapters',[ChapterController::class,'create']);
+Route::put('chapters/{id}',[ChapterController::class,'update']);
+Route::get('chapters',[ChapterController::class,'index']);
+Route::get('chapters/{id}',[ChapterController::class,'show']);
+Route::delete('chapters/{id}',[ChapterController::class,'destroy']);
+
+Route::post('lessons',[LessonController::class,'create']);
+Route::get('lessons',[LessonController::class,'index']);
+Route::put('lessons/{id}',[LessonController::class,'update']);
+Route::get('lessons/{id}',[LessonController::class,'show']);
+Route::delete('lessons/{id}',[LessonController::class,'destroy']);
 
